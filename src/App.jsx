25a60cccc56d9d92fcb21e6b78f5e5a7e6d69085
Karthik Vinod <karthivinu111222@gmail.com>
2026-03-18 12:18:36 +0400

    resume + safari

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Dock, Navbar, Welcome } from "#components";
 import gsap from "gsap";
 import { Draggable } from "gsap/Draggable";
-import Terminal from "#windows";
+import {Terminal, Safari, Resume} from "#windows";
 gsap.registerPlugin(Draggable);
 
 
@@ -14,6 +14,8 @@ const App = () => {
       <Dock />
       
       <Terminal />
+      <Safari />
+      <Resume />
     </main>
   );
 };
