5a533141a55acb419dd5b5363b80e4ffc4920aeb
Karthik Vinod <karthivinu111222@gmail.com>
2026-04-01 20:24:18 +0400

    lifemastery pic update

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -6,13 +6,15 @@ import gsap from "gsap";
 import useWindowStore from "#store/window";
 
 const Dock = () => {
-    const dockRef = useRef(null);
-    const {openWindow,closeWindow, windows} = useWindowStore();
+  const dockRef = useRef(null);
+  const { openWindow, closeWindow, windows } = useWindowStore();
+
   useGSAP(() => {
     const dock = dockRef.current;
     if (!dock) return;
     const icons = dock.querySelectorAll(".dock-icon");
     const { left: dockLeft } = dock.getBoundingClientRect();
+
     const animateIcons = (mouseX) => {
       icons.forEach((icon) => {
         const { left: iconLeft, width: w } = icon.getBoundingClientRect();
@@ -27,11 +29,13 @@ const Dock = () => {
         });
       });
     };
+
     const handleMouseMove = (e) => {
       const { left } = dock.getBoundingClientRect();
       const mouseX = e.clientX - left;
       animateIcons(mouseX);
     };
+
     const resetIcons = () => {
       icons.forEach((icon) => {
         gsap.to(icon, {
@@ -42,27 +46,28 @@ const Dock = () => {
         });
       });
     };
+
     dock.addEventListener("mousemove", handleMouseMove);
     dock.addEventListener("mouseleave", resetIcons);
+
     return () => {
       dock.removeEventListener("mousemove", handleMouseMove);
       dock.removeEventListener("mouseleave", resetIcons);
     };
   }, []);
-    const toggleApp = (app) => {
-        if (!app.canOpen) return;
-        const window = windows[app.id]
-        if (window.isOpen) {
-            closeWindow(app.id)
-        } else {
-            openWindow(app.id)
-        }
+
+  const toggleApp = (app) => {
+    if (!app.canOpen) return;
+    const windowState = windows[app.id];
+    if (windowState.isOpen) closeWindow(app.id);
+    else openWindow(app.id);
   };
+
   return (
     <section id="dock">
       <div ref={dockRef} className="dock-container">
         {dockApps.map(({ id, name, icon, canOpen }) => (
-          <div key={id} className={"relative flex justify-center"}>
+          <div key={id} className="relative flex justify-center">
             <button
               type="button"
               className="dock-icon"
